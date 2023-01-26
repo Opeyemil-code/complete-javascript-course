c97c2153fc479f03f84813731dd46f8ec958fcba
Opeyemil-code <opeyemitoheeb31@gmail.com>
2023-01-25 12:33:33 -1200

    practical

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2037 - birthYear
+
+    function printAge(params) {
+        const output = `you are the ${age}, born in ${birthYear}` 
+        console.log(output)
+        if (birthYear >= 1981 && birthYear < 1996) {
+            const str = `Oh! and you are a mil ${firstName}` 
+             console.log(str)  
+             function add(a,b) {
+                const addTogether = a + b
+                console.log(addTogether)
+                return addTogether;
+             }  
+             add(5,6)
+        }
+    }
+
+    printAge();
+    return age;
+}
+
+const firstName = 'jonas'
+calcAge(1991)
+
+
